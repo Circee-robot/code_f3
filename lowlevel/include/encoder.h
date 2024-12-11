f1224c3ff7c0cf3112cc27320415188b68a6ef8a
benano <soilihi@protonmail.com>
2024-12-11 20:49:27 +0100

    PIN MAP comme sur cubemx je certifie

--- lowlevel/include/encoder.h
+++ lowlevel/include/encoder.h
@@ -1,14 +1,14 @@
 /**
  * @file
- * 
+ *
  * This file is part of cdfr2020BaseRoulanteRework
- * 
- * @brief This implements the setup of the encoders and the counting  
- * 
- * @date  06/2020  
- * 
+ *
+ * @brief This implements the setup of the encoders and the counting
+ *
+ * @date  06/2020
+ *
  * Licence :
- * 
+ *
  * Robotronik Phelma
  * @author PhenixRobotik NPXav Benano Trukbidule
  */
@@ -20,7 +20,7 @@
 #include <libopencm3/stm32/timer.h>
 
 /**
- * number of ticks per turn, encoder 300CPR 
+ * number of ticks per turn, encoder 300CPR
  */
 #define ENCODER_PERIOD (300*4-1)
 
@@ -31,20 +31,20 @@
  *
  * Using one timer and 2 channel for quadrature
  */
-#define ENCODER_A_RCC_TIM RCC_TIM1
-#define ENCODER_A_TIM TIM1
+#define ENCODER_A_RCC_TIM RCC_TIM3
+#define ENCODER_A_TIM TIM3
 
 #define ENCODER_A_INVERSION 0
 
 #define ENCODER_A_CH1_PORT GPIOA
 #define ENCODER_A_CH1_PORT_RCC RCC_GPIOA
-#define ENCODER_A_CH1_AF GPIO_AF6
-#define ENCODER_A_CH1_PIN GPIO8
+#define ENCODER_A_CH1_AF GPIO_AF2
+#define ENCODER_A_CH1_PIN GPIO6
 
 #define ENCODER_A_CH2_PORT GPIOA
 #define ENCODER_A_CH2_PORT_RCC RCC_GPIOA
-#define ENCODER_A_CH2_AF GPIO_AF6
-#define ENCODER_A_CH2_PIN GPIO9
+#define ENCODER_A_CH2_AF GPIO_AF2
+#define ENCODER_A_CH2_PIN GPIO7
 /** @} */
 
 /**
@@ -72,7 +72,7 @@
 
 /**
  * @brief enum of the two encoders of the robot to choose which one will be used
- * 
+ *
  */
 enum encoder_sel {ENCODER_A, ENCODER_B};
 
@@ -84,7 +84,7 @@ void encoder_setup();
 
 /**
  * @brief get the actual counter on selected encoder
- * 
+ *
  * @param sel    the encoder for which the counter is read
  * @return    actual counter on selected encoder
  */
@@ -92,7 +92,7 @@ int _encoder_get_counter(enum encoder_sel sel);
 
 /**
  * @brief delta steps in small time difference
- * 
+ *
  * @param sel    the encoder that is updated
  * @param prev_count    pointer on the previous
  * @return    delta value (difference between actual and previous)
